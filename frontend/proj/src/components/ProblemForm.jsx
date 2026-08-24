729db96df62eeda3037a77ce49f95e294677fe2e
vanshika <vanshikabindal2@gmail.com>
2026-08-24 19:21:10 +0530

    fix problem categories import

--- frontend/proj/src/components/ProblemForm.jsx
+++ frontend/proj/src/components/ProblemForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import problemCategories from "../data/problemCategories";
+import problemCategories from "../data/problemCategories.jsx";
 import { getDepartmentByCategory } from "../utlis/DepartmentUtils";
 import { createComplaint } from "../api/complaintApi";
 
